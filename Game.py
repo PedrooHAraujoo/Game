3b219411abcd8b17026275e8b30145b973687762
Pedro Henrique Araújo Souza <161653851+PedrooHAraujoo@users.noreply.github.com>
2024-07-29 17:31:56 -0300

    Código funcional, mas com uma falha quando o user coloca mais de uma letra de uma vez.

--- Game.py
+++ Game.py
@@ -1,6 +1,6 @@
 print('Bem Vindo ao Jogo da Forca!')
-palavra_secreta = input('Digite a palavra secreta: ')
-lista_de_acertos = [palavra_secreta]
+palavra_secreta = input('Digite a palavra secreta: ').lower().strip()
+lista_de_acertos = ['_' for _ in palavra_secreta]
 tentativas = 6
 listas_de_letras_tentadas = []
 def palavra_descoberta(lista_de_acertos):
@@ -9,17 +9,17 @@ def palavra_descoberta(lista_de_acertos):
 while tentativas > 0 and not palavra_descoberta(lista_de_acertos):
     print(' '.join(lista_de_acertos))
     print(tentativas)
-    print(listas_de_letras_tentadas)
+    print(','.join(listas_de_letras_tentadas))
 
-    letra_do_jogador = input("Digite uma letra: ")
+    letra_do_jogador = input("Digite uma letra: ").lower().strip()
 
     if letra_do_jogador in listas_de_letras_tentadas:
         print('Você já tentou essa letra, tente novamente.')
     else:
         listas_de_letras_tentadas.append(letra_do_jogador)
         if letra_do_jogador in palavra_secreta:
-            for indice, letra_do_jogador in enumerate(palavra_secreta):
-                if palavra_secreta[indice] == letra_do_jogador:
+            for indice, letra in enumerate(palavra_secreta):
+                if letra == letra_do_jogador:
                     lista_de_acertos[indice] = letra_do_jogador
         else:
             tentativas -= 1
